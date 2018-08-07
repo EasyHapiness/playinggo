4f286587dcd56383f22c9836fb47df88691ba434
EasyHapiness <1229081051@qq.com>
2018-08-07 11:08:21 +0800

    新增日志打印

--- src/main/java/com/yasong/yin/playinggo/controller/LoginController.java
+++ src/main/java/com/yasong/yin/playinggo/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.yasong.yin.playinggo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ResponseBody
 @RequestMapping
 public class LoginController extends BaseController{
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping("/login")
     public Object login(){
