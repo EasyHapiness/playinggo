f72aeb324fd5176d66eaede993415bdc5ab853fe
1229081051@qq.com <songloveqin1314>
2019-03-01 16:19:44 +0800

    新增Redis 实现

--- src/main/java/com/yasong/yin/playinggo/redis/RedisConfig.java
+++ src/main/java/com/yasong/yin/playinggo/redis/RedisConfig.java
@@ -18,19 +18,12 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
 
         RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
-
         template.setConnectionFactory(factory);
-
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-
         ObjectMapper om = new ObjectMapper();
-
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-
         om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-
         jackson2JsonRedisSerializer.setObjectMapper(om);
-
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
 
         // key采用String的序列化方式
